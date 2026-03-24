d75318bec5ad10a41905fe3efaf632c272caefae
waimea-gkidd <gekidd@waimea.school.nz>
2026-03-25 12:33:02 +1300

    Removed unneeded things
    I removed the bits of code I wont be using mainly linked to score which I do not need for my game. Added the locations for my game. First step is just getting place to place working.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -22,22 +22,40 @@ fun main() {
  * @property name the user's name
  * @property score the points earned
  */
+class Location(
+    val name: String,
+    val description: String
+)
 class App {
-    var name = "Test"
-    var score = 0
+    val locations = mutableListOf<Location>()
+    var currentLocation: Location
 
-    fun scorePoints(points: Int) {
-        score += points
-    }
+    init {
+        val townCentre = Location("Town Centre", "The centre of town.")
+        val scummBar = Location("Scumm Bar", "A noisy pirate bar.")
+        val generalStore = Location("General Store", "A shop with odd items.")
+        val jail = Location("Jail", "A small stone jail.")
+        val alley = Location("Alley", "A narrow alley with stray dogs.")
+        val storageYard = Location("Storage Yard", "A yard full of crates.")
+        val clockTowerBase = Location("Clock Tower Base", "The bottom of the old tower.")
+        val topOfClockTower = Location("Top of Clock Tower", "The top of the tower.")
+        val mayorsMansion = Location("Mayor's Mansion", "A large locked mansion.")
 
-    fun resetScore() {
-        score = 0
-    }
+        locations.add(townCentre)
+        locations.add(scummBar)
+        locations.add(generalStore)
+        locations.add(jail)
+        locations.add(alley)
+        locations.add(storageYard)
+        locations.add(clockTowerBase)
+        locations.add(topOfClockTower)
+        locations.add(mayorsMansion)
 
-    fun maxScoreReached(): Boolean {
-        return score >= 10000
+        currentLocation = townCentre
     }
 }
+
+
 
 
 /**
@@ -46,16 +64,12 @@ class App {
  * @param app the app state object
  */
 class MainWindow(val app: App) {
-    val frame = JFrame("WINDOW TITLE")
+    val frame = JFrame("Meelé Island")
     private val panel = JPanel().apply { layout = null }
 
-    private val titleLabel = JLabel("APP TITLE")
+    private val titleLabel = JLabel("Meelé island explorer")
 
     private val infoLabel = JLabel()
-    private val clickButton = JButton("Click Me!")
-    private val infoButton = JButton("Info")
-
-    private val infoWindow = InfoWindow(this, app)      // Pass app state to dialog too
 
     init {
         setupLayout()
@@ -70,23 +84,16 @@ class MainWindow(val app: App) {
 
         titleLabel.setBounds(30, 30, 340, 30)
         infoLabel.setBounds(30, 90, 340, 30)
-        clickButton.setBounds(30, 150, 240, 40)
-        infoButton.setBounds(300, 150, 70, 40)
 
         panel.add(titleLabel)
         panel.add(infoLabel)
-        panel.add(clickButton)
-        panel.add(infoButton)
+
     }
 
     private fun setupStyles() {
         titleLabel.font = Font(Font.SANS_SERIF, Font.BOLD, 32)
         infoLabel.font = Font(Font.SANS_SERIF, Font.PLAIN, 20)
 
-        clickButton.font = Font(Font.SANS_SERIF, Font.PLAIN, 20)
-        clickButton.background = Color(0xcc0055)
-
-        infoButton.font = Font(Font.SANS_SERIF, Font.PLAIN, 20)
     }
 
     private fun setupWindow() {
@@ -98,31 +105,12 @@ class MainWindow(val app: App) {
     }
 
     private fun setupActions() {
-        clickButton.addActionListener { handleMainClick() }
-        infoButton.addActionListener { handleInfoClick() }
+
     }
 
-    private fun handleMainClick() {
-        app.scorePoints(1000)       // Update the app state
-        updateUI()                  // Update this window UI to reflect this
-    }
-
-    private fun handleInfoClick() {
-        infoWindow.show()
-    }
 
     fun updateUI() {
-        infoLabel.text = "User ${app.name} has ${app.score} points"
 
-        if (app.maxScoreReached()) {
-            clickButton.text = "No More!"
-            clickButton.isEnabled = false
-        } else {
-            clickButton.text = "Click Me!"
-            clickButton.isEnabled = true
-        }
-
-        infoWindow.updateUI()       // Keep child dialog window UI up-to-date too
     }
 
     fun show() {
@@ -138,66 +126,3 @@ class MainWindow(val app: App) {
  * @param owner the parent frame, used to position and layer the dialog correctly
  * @param app the app state object
  */
-class InfoWindow(val owner: MainWindow, val app: App) {
-    private val dialog = JDialog(owner.frame, "DIALOG TITLE", false)
-    private val panel = JPanel().apply { layout = null }
-
-    private val infoLabel = JLabel()
-    private val resetButton = JButton("Reset")
-
-    init {
-        setupLayout()
-        setupStyles()
-        setupActions()
-        setupWindow()
-        updateUI()
-    }
-
-    private fun setupLayout() {
-        panel.preferredSize = java.awt.Dimension(240, 180)
-
-        infoLabel.setBounds(30, 30, 180, 60)
-        resetButton.setBounds(30, 120, 180, 30)
-
-        panel.add(infoLabel)
-        panel.add(resetButton)
-    }
-
-    private fun setupStyles() {
-        infoLabel.font = Font(Font.SANS_SERIF, Font.PLAIN, 16)
-        resetButton.font = Font(Font.SANS_SERIF, Font.PLAIN, 16)
-    }
-
-    private fun setupWindow() {
-        dialog.isResizable = false                              // Can't resize
-        dialog.defaultCloseOperation = JDialog.HIDE_ON_CLOSE    // Hide upon window close
-        dialog.contentPane = panel                              // Main content panel
-        dialog.pack()
-    }
-
-    private fun setupActions() {
-        resetButton.addActionListener { handleResetClick() }
-    }
-
-    private fun handleResetClick() {
-        app.resetScore()    // Update the app state
-        owner.updateUI()    // Update the UI to reflect this, via the main window
-    }
-
-    fun updateUI() {
-        // Use app properties to display state
-        infoLabel.text = "<html>User: ${app.name}<br>Score: ${app.score} points"
-
-        resetButton.isEnabled = app.score > 0
-    }
-
-    fun show() {
-        val ownerBounds = owner.frame.bounds          // get location of the main window
-        dialog.setLocation(                           // Position next to main window
-            ownerBounds.x + ownerBounds.width + 10,
-            ownerBounds.y
-        )
-
-        dialog.isVisible = true
-    }
-}
